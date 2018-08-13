c4562f500245e60a32d4d470535c2af0e25a0e3c
Caoimhín Laoide-Kemp <laoidekc@tcd.ie>
2018-08-13 16:37:41 +0100

    Removed hardcoded files and added argument checks

--- idlak-misc/cart_lts/run.sh
+++ idlak-misc/cart_lts/run.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
+# Takes a lexicon a generates letter-to-sound rules
 
+if [ $# -ne 2 ]
+    then
+        echo "Usage is "$0" <input lexicon> <output file>"
+        exit 1
+    else
+        lexicon=$1
+        output=$2
+fi
 
-lexicon="ga_ie_default.lex" 
+if [[ ${lexicon: -4} != ".lex" ]]
+    then
+        echo "Lexicon must be a .lex file"
+        exit 1
+fi
 
-echo "****() Converting lexicon"
-(
-cat "$lexicon" | cut -d " " -f 1-2 > converted.lex
-)
+align="${lexicon::-4}"".align"
+converted="converted.lex"
 
-phonet2cartltspath="../../idlak-misc/cart_lts/"
+mkdir -p tmp
+phonet2cartltspath="../../idlak-misc/cart_lts/tmp/"
+
+echo "#####Converting lexicon#####"
+cat "$lexicon" | cut -d " " -f 1-2 > tmp/converted.lex
 
 cd ~/idlak/tools
 
-echo "****() Installing Phonetisaurus"
-(
+echo "#####Installing Phonetisaurus#####"
 if [ ! -d "Phonetisaurus" ]
 then
     git clone https://github.com/AdolfVonKleist/Phonetisaurus.git
@@ -28,14 +42,27 @@ then
 else
     echo "Phonetisaurus already installed"
 fi
-)
 
-echo "****() Creating alignment file"
-(
+echo "#####Creating alignment file#####"
 cd Phonetisaurus
 ./phonetisaurus-align \
---input="$phonet2cartltspath""converted.lex" \
---ofile="$phonet2cartltspath""ga.align" \
+--input="$phonet2cartltspath""$converted" \
+--ofile="$phonet2cartltspath""$align" \
 --delim=" " \
 --s2_char_delim="_"
-)
+
+echo "#####Generating cart files#####"
+cd "$phonet2cartltspath"
+python ../phonet2cart.py "$align"
+
+echo "#####Running wagon#####"
+for f in *.cart
+do
+    ID=${f::-5}
+    mkdir -p cart/"$ID"
+    wagon -desc wagon_description.dat -data "$f" -o cart/"$ID"/tree.dat
+done
+
+echo "#####Generating lts rules#####"
+python ../carttree2xml.py cart ccart-default.xml
+mv ccart-default.xml ..
