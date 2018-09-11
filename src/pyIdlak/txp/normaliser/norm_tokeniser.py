d935f79dbdd5d997e46070b187c4cdd2b2616838
Skaiste <butkute.skaiste@gmail.com>
2018-09-11 13:59:44 +0100

    removed all but first values in normalised tokens

--- src/pyIdlak/txp/normaliser/norm_tokeniser.py
+++ src/pyIdlak/txp/normaliser/norm_tokeniser.py
@@ -10,9 +10,9 @@ def splitNormalised(token):
         token.set('norm', token.get('nnorm'))
         token.attrib.pop('nnorm')
         return
-    
+
     words = token.get('nnorm').split(' ')
-    
+
     # replace the original token norm with first word
     token.set('norm', words[0])
     # remove the nnorm attribute
@@ -24,32 +24,33 @@ def splitNormalised(token):
         # make a copy of original token
         new_tk = deepcopy(token)
         new_tk.set('norm', w)
+        new_tk.text = ""
         # append it after the previous token
         last_index = tk_parent.index(last_tk)
         tk_parent.insert(last_index+1, new_tk)
         last_tk = new_tk
-        
+
 
 class Norm_Tokenise(object):
-    
+
     def __init__(self, idargs):
         if type(idargs) != idargparse.TxpArgumentParser:
             raise ValueError("idargs must be a TxpArgumentParser")
-            
-        
+
+
     def process(self, doc):
         """ process the document in place """
         if not type(doc) is xmldoc.XMLDoc:
             raise ValueError("doc must be a XMLDoc")
-        
+
         # xmldoc to lxml
         strin = doc.to_string()
         xmlin = etree.fromstring(strin)
-        
+
         tokens = xmlin.xpath('.//tk|.//break')
         for t in tokens:
             splitNormalised(t)
-        
+
         # lxml to xmldoc
         strout = str(etree.tostring(xmlin, encoding='utf8').decode('utf8'))
         doc.load_string(strout)
