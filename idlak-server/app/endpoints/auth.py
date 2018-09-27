12d7f673ff7035ed6186d4771abe9826711f04fd
Skaiste <butkute.skaiste@gmail.com>
2018-09-27 09:41:28 +0100

    added a token expiration endpoint and script for setting up the server

--- idlak-server/app/endpoints/auth.py
+++ idlak-server/app/endpoints/auth.py
@@ -1,44 +1,82 @@
-from app import api, jwt
+import flask_jwt_simple
+from app import app, api, jwt
 from app.models.user import User
+from app.middleware.auth import not_expired, EXPIRED
 from flask import jsonify
 from flask_restful import Resource, reqparse, abort, request
-from flask_jwt_simple import create_jwt
+from flask_jwt_simple import create_jwt, decode_jwt, jwt_required
 from passlib.hash import pbkdf2_sha256 as sha256
 
-""" login arguments: 
+""" login arguments:
     uid (str) : user id
     password (str) : user password """
 usr_parser = reqparse.RequestParser()
 usr_parser.add_argument('uid', help='user id', location='json', required=True)
-usr_parser.add_argument('password', help='user password', location='json', required=True)
+usr_parser.add_argument('password', help='user password', location='json',
+                        required=True)
+
 
 class Auth(Resource):
     def post(self):
         """ Authentication endpoint
-        
+
             Args:
                 uid (str) : user id
                 password (str) : user password
             Returns:
                 str: access token if login details are correct, otherwise
-                returns an error message 
+                returns an error message
         """
         args = usr_parser.parse_args()
         user = User.query.get(args['uid'])
         if user and sha256.verify(args['password'], user.password):
             return {'access_token': create_jwt(identity=user.id)}, 200
-        return { "message" : "Login details are incorrect" }, 401
+        return {"message": "Login details are incorrect"}, 401
+
+
+class Auth_Expire(Resource):
+    decorators = [not_expired, jwt_required]
+
+    def post(self):
+        """ Expire token endpoint
+
+            Args:
+                access_token (str) : access token from header
+            Returns:
+                str: success or error message
+        """
+        # get access token from header
+        header_name = app.config['JWT_HEADER_NAME']
+        jwt_header = request.headers.get(header_name, None)
+        if len(jwt_header.split()) == 1:
+            access_token = jwt_header
+        elif len(jwt_header.split()) == 2:
+            access_token = jwt_header.split()[1]
+
+        user_id = decode_jwt(access_token)
+
+        if user_id is not None:
+            EXPIRED.append(access_token)
+            app.logger.info("Expire token for user " + user_id['sub'])
+            return {"message": "The token has been manually expired."}, 200
+        else:
+            return {"message": "The token could not expire."}, 400
+
 
 api.add_resource(Auth, '/auth')
+api.add_resource(Auth_Expire, '/auth/expire')
+
 
 @jwt.expired_token_loader
 def expired_token():
-    return jsonify({ "message":"Access token has expired" }), 401
+    return jsonify({"message": "Access token has expired"}), 401
+
 
 @jwt.invalid_token_loader
 def invalid_token(error_msg):
-    return jsonify({ "message":"Access token is invalid" }), 401
+    return jsonify({"message": "Access token is invalid"}), 401
+
 
 @jwt.unauthorized_loader
 def unauthorized_token(error_msg):
-    return jsonify({ "message":"Access token is invalid" }), 401
+    return jsonify({"message": "Access token is invalid"}), 401
--- idlak-server/app/models/user.py
+++ idlak-server/app/models/user.py
@@ -12,7 +12,7 @@ class User(db.Model):
 
     def __init__(self, id, password, admin):
         """ __init__ method for User class.
-        
+
         Args:
             id (str): Id of the user.
             password (str): user password.
@@ -25,15 +25,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}:{}:{}>'.format(self.id, self.password, self.admin)
 
-    
     @classmethod
     def new_user(self, isAdmin=False, user_id=uuid.uuid4().hex[:16]):
         """ Creates a new user.
-        
+
         Args:
             isAdmin (bool, optional): if user to be created is an admin.
             user_id (str, optional): user id.
-            
+
         Returns:
             :obj:'User': info of the new user (with id and password).
         """
@@ -52,20 +51,21 @@ class User(db.Model):
                          "\tpassword: {},\n"
                          "\tencrypted-password: {},\n"
                          "\tadmin: {}\n}}"
-                         .format(user.id, user_pass, user.password, user.admin))
+                         .format(user.id, user_pass, user.password,
+                                 user.admin))
         # create user with the unhashed password
         user = User(user_id, user_pass, isAdmin)
         return user
-    
+
     @classmethod
     def new_user_full(self, user_id, user_pass, admin):
         """ Creates a new user provided all details.
-        
+
         Args:
             admin (bool): if user to be created is an admin.
             user_id (str): user id.
             user_pass (str): user password.
-            
+
         Returns:
             :obj:'User': info of the new user (with id and password).
         """
@@ -80,14 +80,15 @@ class User(db.Model):
                          "\tpassword: {},\n"
                          "\tencrypted-password: {},\n"
                          "\tadmin: {}\n}}"
-                         .format(user.id, user_pass, user.password, user.admin))
+                         .format(user.id, user_pass, user.password,
+                                 user.admin))
         # create user with the unhashed password
         user = User(user_id, user_pass, admin)
         return user
 
     def generate_new_pass(self):
         """ Created new password for a user.
-        
+
         Returns:
             str: the new generated password.
         """
@@ -98,16 +99,17 @@ class User(db.Model):
         self.password = hashed_pass
         db.session.merge(self)
         db.session.commit()
-        app.logger.debug("Password for user {} has been changed into {}".format(self.id, user_pass))
+        app.logger.debug("Password for user {} has been changed into {}"
+                         .format(self.id, user_pass))
         return user_pass
-    
-    
+
     def toggle_admin(self):
         """ Toggles users admin status """
         self.admin = not self.admin
         db.session.merge(self)
         db.session.commit()
-        app.logger.debug("Admin status for user {} has been changed into {}".format(self.id, self.admin))
+        app.logger.debug("Admin status for user {} has been changed into {}"
+                         .format(self.id, self.admin))
 
     def delete(self):
         """ Deletes user from a database. """
@@ -118,13 +120,13 @@ class User(db.Model):
 
 def authenticate(user_id, password):
     """ Checks if user exists and its password is correct.
-    
+
     Used for the jwt module.
-    
+
     Args:
         user_id (str): The user id provided on login.
         password (str): The user password provided on login.
-    
+
     Returns:
         :obj:'User': user that has matching id and correct password.
     """
@@ -132,17 +134,18 @@ def authenticate(user_id, password):
     if user and sha256.verify(password, user.password):
             return user
 
+
 def identity(payload):
     """ Extracts identity of the user from the payload.
-    
+
     Used for the jwt module.
-    
+
     Args:
-        payload (dict): a payload object that must have an identity key value pair.
-    
+        payload (dict): a payload object that must have
+                        an identity key value pair.
+
     Returns:
         :obj:'User': the user that is found by the identity in the payload.
     """
     user_id = payload['identity']
     return User.query.get(user_id)
-
