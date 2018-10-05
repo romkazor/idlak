446ab0957e32166ae1791680f5002f0a1ae169c4
Skaiste <butkute.skaiste@gmail.com>
2018-10-05 14:14:47 +0100

    trying to make nnet-forward wrapper - wip

---
+++ src/pyIdlak/nnet-forward/pyIdlak-nnet-forward.h
@@ -0,0 +1,10 @@
+#include <limits>
+
+#include "nnet/nnet-nnet.h"
+#include "nnet/nnet-loss.h"
+#include "nnet/nnet-pdf-prior.h"
+#include "base/kaldi-common.h"
+#include "util/common-utils.h"
+#include "base/timer.h"
+
+int ForwardPass(int argc, char *argv[]);
