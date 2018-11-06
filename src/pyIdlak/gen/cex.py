faa5890b450ed646f0023ed995c575244147d268
David Braude <dabraude@gmail.com>
2018-11-06 15:54:57 +0000

    Idlak now can use the CEX parser at all stages.

--- src/pyIdlak/gen/cex.py
+++ src/pyIdlak/gen/cex.py
@@ -19,16 +19,20 @@ import sys
 import re
 import json
 import collections
+import logging
 from lxml import etree
 
-from .. import txp
-from .. import pylib
 
 CexFunc = collections.namedtuple('CexFunc', ['name', 'id', 'delim', 'isinteger'])
 CexConversion = collections.namedtuple('CexConversion', ['id', 'mapping', 'func', 'args'])
 
+# TODO compare the CEX to the arch
+
 class CEXParser:
-    def __init__(self, cexspec, include_phon_val = True):
+
+    def __init__(self, cexspec, include_phon_val = True, loglvl = logging.WARN):
+        logging.basicConfig(level = loglvl)
+        self.log = logging.getLogger('cexparse')
         if type(cexspec) == str:
             _cexspec = etree.parse(cexspec)
         elif (type(cexspec) not in [etree._Element, etree._ElementTree]):
@@ -39,6 +43,67 @@ class CEXParser:
         self._conversions = None
         self._include_phon_val = bool(include_phon_val)
         self._parse_header(_cexspec)
+        self._kaldi_phone_pat = re.compile('\^(.*?)\~(.*?)\-(.*?)\+(.*?)\=(.*)')
+        self._hts_phone_pat = re.compile('(.*?)\^(.*?)\-(.*?)\+(.*?)\=(.*)')
+        # cex000 is always phone identity or missing
+        self._cex000func = CexFunc('PhoneVal', 'cex000', None, False)
+        self.log.info("CEX parser initialised")
+
+
+
+    def parse(self, xml, check_pron_strings = False):
+        """ Parse the CEX document using the parser and returns then
+            features by fileid / utterances
+
+            if check_pron_strings is set to True, the parser will check
+            the the strings in pron tags contain valid phone information
+            in either Kaldi or HTS form
+        """
+        if (type(xml) not in [etree._Element, etree._ElementTree]):
+            raise ValueError('CEX spec is not an etree element or string')
+
+        self._parse_err = False
+        self._untitled_counter = 1
+        cex_features = {}
+        if xml.find('fileid') is not None:
+            self.log.info('iterating over file ids')
+            spurt_iterator = xml.iter('fileid')
+        else:
+            self.log.info('iterating over spurts')
+            spurt_iterator = xml.iter('spt')
+
+        for spurt in spurt_iterator:
+            spurt_id = spurt.attrib.get('id')
+            if spurt_id is None:
+                spurt_id = self.default_name +'{0:03d}'.format(self._untitled_counter)
+                self._untitled_counter += 1
+            self.log.debug('spurt ID is {0}'.format(spurt_id))
+            cex_features[spurt_id] = self._parse_spurt(spurt, check_pron_strings)
+
+        return cex_features
+
+
+    def calculate_freqtable(self, cex_features):
+        """ Takes the output from parsing and creates a frequency table """
+        cexfreqtable = {}
+        for cexid in self._cexids:
+            cexfreqtable[cexid] = {}
+        for fileid, file_cexfeatures in cex_features.items():
+            for phone_cex in file_cexfeatures:
+                for val, cexid in zip(phone_cex, self._cexids):
+                    if val in cexfreqtable[cexid]:
+                        cexfreqtable[cexid][val] += 1
+                    else:
+                        cexfreqtable[cexid][val] = 1
+        # fill in missing values for integers
+        for cexid in self._cexids:
+            if self._get_cexfunc(cexid).isinteger:
+                minval = min(cexfreqtable[cexid].keys())
+                maxval = max(cexfreqtable[cexid].keys())
+                for i in range(minval, maxval): # no need to include maxval
+                    if i not in cexfreqtable[cexid]:
+                        cexfreqtable[cexid][i] = 0
+        return cexfreqtable
 
 
     def set_conversions(self, cexfreqtable, norm_ints = True):
@@ -46,29 +111,11 @@ class CEXParser:
         self._conversions = {}
 
         for cexid, val_freqs in cexfreqtable.items():
-            if cexid == 'cex000':
-                 # cex000 is always phone identity or missing
-                cexfunc = CexFunc('PhoneVal', 'cex000', None, False)
-            else:
-                cexfunc = [c for c in self._cexfunctions if c.id == cexid][0]
-
-            def _isint(v):
-                try:
-                    int(v)
-                    return True
-                except:
-                    return False
+            cexfunc = self._get_cexfunc(cexid)
 
             cex_values = list(val_freqs.keys())
-            all_int = all(map(_isint, cex_values))
-            if all_int:
+            if cexfunc.isinteger:
                 cex_values = list(map(int, cex_values))
-            else:
-                if cexfunc.isinteger:
-                    raise ValueError('Non integer value in integer cex feature frequency table: '+ cexid)
-            cex_values.sort()
-
-            if all_int:
                 mapping = False
                 if norm_ints:
                     func = self._norm_int
@@ -77,6 +124,7 @@ class CEXParser:
                     func = False
                     args = None
             else:
+                cex_values.sort()
                 mapping = {}
                 idx = 1
                 arrlen = len(cex_values)
@@ -91,31 +139,6 @@ class CEXParser:
                 args = [arrlen]
 
             self._conversions[cexid] = CexConversion(cexid, mapping, func, args)
-
-
-
-    def parse(self, xml):
-        """ Parse the CEX document using the parser and returns then
-            features by fileid / utterances """
-        if (type(xml) not in [etree._Element, etree._ElementTree]):
-            raise ValueError('CEX spec is not an etree element or string')
-
-        self._parse_err = False
-        self._untitled_counter = 1
-        cex_features = {}
-        if xml.find('filedid'):
-            spurt_iterator = xml.iter('fileid')
-        else:
-            spurt_iterator = xml.iter('spt')
-
-        for spurt in spurt_iterator:
-            spurt_id = spurt.attrib.get('id')
-            if spurt_id is None:
-                spurt_id = self.default_name +'{0:03d}'.format(self._untitled_counter)
-                self._untitled_counter += 1
-            cex_features[spurt_id] = self._parse_spurt(spurt)
-
-        return cex_features
 
 
     def convert_to_dnnfeatures(self, cex_features):
@@ -147,8 +170,7 @@ class CEXParser:
             isinteger = bool(int(cexfunc.attrib.get('isinteger', 0)))
             self._cexfunctions.append(CexFunc(name, cexid, delim, isinteger))
 
-        # Build a regex for extracting the context from the phone tags
-        # get the ctxid for getting the  phone name
+        # Build a regex for extracting the context from the phone tag
         pat = ''
         for cexfunc in self._cexfunctions:
             pat += cexfunc.delim + '(?P<' + cexfunc.id + '>'
@@ -161,9 +183,12 @@ class CEXParser:
                 pat += '?'
             pat += ')'
         self._repat = re.compile(pat)
+        self._cexids = []
+        if self._include_phon_val:
+            self._cexids.append('cex000')
+        self._cexids.extend([c.id for c in self._cexfunctions])
 
-
-    def _parse_spurt(self, spurt):
+    def _parse_spurt(self, spurt, check_pron_strings):
         """ Parse an indiviual spurt / fileid """
         phone_name_pre = ''
         cexfeats = []
@@ -176,16 +201,22 @@ class CEXParser:
                 continue
             phone_name_pre = phone_name
 
-            cexfeats.append(self._pron_to_feat(pron))
+            cexfeats.append(self._pron_to_feat(pron, check_pron_strings))
         return cexfeats
 
 
-    def _pron_to_feat(self, pron):
+    def _pron_to_feat(self, pron, check_pron_strings):
         """ Converts a pron tag to context features """
+        if check_pron_strings:
+            if self._kaldi_phone_pat.match(pron.text) is None \
+                and self._hts_phone_pat.match(pron.text) is None:
+                    msg = 'phone context string does not match Kaldi or HTS formats'
+                    msg += str(pron.text)
+                    self.log.critical(msg)
         m = self._repat.match(pron.text)
         if m is None:
             self._parse_err = True
-            print("Bad phone context string: ", pron.text, file=sys.stderr)
+            self.log.error("Bad phone context string: " + str(pron.text))
         phone_name = pron.attrib.get('val', 'pau')
         cexs = []
         if self._include_phon_val:
@@ -203,22 +234,18 @@ class CEXParser:
         if not self._conversions:
             raise ValueError('Cannot convert to DNN features if coversion has not been set')
 
-        if self._include_phon_val:
-            cexids = ['cex000'] + [c.id for c in self._cexfunctions]
-        else:
-            cexids = [c.id for c in self._cexfunctions]
-
         spurt_features = []
         for phone_cex in cex_features:
             phone_features = []
-            for cex, cexid in zip(phone_cex, cexids):
+            for cex, cexid in zip(phone_cex, self._cexids):
                 conversion = self._conversions[cexid]
                 if conversion.mapping:
                     try:
                         val = conversion.mapping[cex]
                     except KeyError:
-                        print("Bad unknown value '{0}' for context feature '{1}' in context string: ".
-                              format(cex, cexid), ' '.join(phone_cex), file=sys.stderr)
+                        msg = "Bad unknown value '{0}' for context feature '{1}' in context string: ".format(cex, cexid)
+                        msg += ' '.join(phone_cex)
+                        self.log.error(msg)
                         val = 0
                 else:
                     val = cex
@@ -231,7 +258,6 @@ class CEXParser:
             spurt_features.append(phone_features)
 
         return spurt_features
-
 
 
     def _to_binary_array(self, val, arrlen):
@@ -247,6 +273,15 @@ class CEXParser:
         """ Converts integer to float using offset and scaling """
         return [(val - offset) / float(scaling)]
 
+    def _get_cexfunc(self, cexid):
+        """ Get the associated CEX function for the ID """
+        if cexid == 'cex000':
+            return self._cex000func
+        for c in self._cexfunctions:
+            if c.id == cexid:
+                return c
+        self.log.error("Cannot find function for CEX id: {0}".format(cexid))
+        return None
 
 
 ###############################################################
@@ -254,8 +289,8 @@ class CEXParser:
 def load_cexfreqtable(fname):
     """ Loads a Context Feature frequency table """
     with open(fname) as fp:
-        freqtables = json.load(fp)
-    return freqtables
+        cexfreqtable = json.load(fp)
+    return cexfreqtable
 
 
 def save_cexfreqtable(fname, cexfreqtable):
@@ -266,6 +301,7 @@ def save_cexfreqtable(fname, cexfreqtable):
 
 def cex_to_feat(doc, cexfreqtable):
     """ Converts XML with context features to dnn features """
+    from .. import txp
     if not type(doc) == txp.XMLDoc:
         raise ValueError("doc must be a txp XMLDoc")
 
@@ -301,7 +337,3 @@ def feat_to_ark(fname, dnn_features):
         fout.flush()
     else:
         fout.close()
-
-
-
-
