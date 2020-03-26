b70122f773e8bd10ee63c87ccb800c078540623b
Skaiste Butkute <butkute.skaiste@gmail.com>
2020-03-26 11:25:34 +0000

    adjusted setup server script

--- idlak-server/setup-server.sh
+++ idlak-server/setup-server.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
+set -v
 TOREPLACE="from pip import main"
 REPLACETO="from pip._internal import main"
 
-echo "virtualenv -p python3 venv"
 virtualenv -p python3 venv
-echo "source venv/bin/activate"
 source venv/bin/activate
-echo "replace \"$TOREPLACE\" \"$REPLACETO\" -- venv/bin/pip3"
 replace "$TOREPLACE" "$REPLACETO" -- venv/bin/pip3
-echo "venv/bin/pip3 install -r requirements.txt"
-venv/bin/pip3 install -r requirements.txt
-echo "export FLASK_APP=runserver.py"
+venv/bin/python3 -m pip install -r requirements.txt
 export FLASK_APP=runserver.py
+set +v
